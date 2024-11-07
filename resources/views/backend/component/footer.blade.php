3cc35e38c395e93d94ca02fb9de60da2c444ca78
Zalna Mustika <109508971+zalnamustika11@users.noreply.github.com>
2024-11-07 21:41:23 +0700

    up

--- resources/views/backend/component/footer.blade.php
+++ resources/views/backend/component/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy;
-                <a href="{{ App\Models\Setting::where('slug', 'website-url')->get()->first()->description }}" target="_blank">{{ App\Models\Setting::where('slug', 'nama-toko')->get()->first()->description }}</a> {{ date('Y') }}
+            <div class="text-muted"> &copy;
+                 {{ date('Y') }}
             </div>
             <div>
                 {{-- <a href="#">Privacy Policy</a>
